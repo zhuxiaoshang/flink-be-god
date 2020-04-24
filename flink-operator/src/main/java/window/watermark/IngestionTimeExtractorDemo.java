80c943a4bfcbf28b8e9bd685bb2700606ee96752
18054869 <18054869@cnsuning.com>
2020-04-24 18:02:39 +0800

    add watermark demo

---
+++ flink-operator/src/main/java/window/watermark/IngestionTimeExtractorDemo.java
@@ -0,0 +1,24 @@
+package window.watermark;
+
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.IngestionTimeExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import window.datasource.SourceGenerator;
+import window.function.ApplyWindowFunction;
+
+/**
+ * 基于摄取时间语义的水印生成器,默认获取机器时钟作为水印时间戳。
+ */
+public class IngestionTimeExtractorDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);//这里时间语义不起作用
+        DataStream<Tuple3<String, Integer, Long>> src =
+                env.addSource(new SourceGenerator()).setParallelism(1).assignTimestampsAndWatermarks(new IngestionTimeExtractor<>());
+        src.keyBy(0).timeWindow(Time.seconds(5)).apply(new ApplyWindowFunction()).print();
+        env.execute();
+    }
+}
