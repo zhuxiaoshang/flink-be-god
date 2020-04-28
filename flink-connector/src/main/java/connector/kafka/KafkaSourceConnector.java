546118918e0e25ed99d9e99650591927ccc4b8a4
18054869 <18054869@cnsuning.com>
2020-04-28 09:11:08 +0800

    add connector demo

--- flink-connector/src/main/java/connector/kafka/KafkaSourceConnector.java
+++ flink-connector/src/main/java/connector/kafka/KafkaSourceConnector.java
@@ -23,18 +23,17 @@ public class KafkaSourceConnector {
 
     public static DataStream<String> getKafkaSource(StreamExecutionEnvironment env) {
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "kafkasit02broker01.cnsuning.com:9092,kafkasit02broker02.cnsuning.com:9092,kafkasit02broker03.cnsuning.com:9092");
-        properties.setProperty("zookeeper.connect", "kafkasit02zk01.cnsuning.com:2181,kafkasit02zk02.cnsuning.com:2181,kafkasit02zk03.cnsuning.com:2181");
-        properties.setProperty("group.id", "bi_dfp_order_rank_list_dfp");
+        properties.setProperty("bootstrap.servers", "broker");
+        properties.setProperty("zookeeper.connect", "zk");
+        properties.setProperty("group.id", "groupid");
         //开始消费的offset位置，支持latest, earliest, none
         properties.setProperty("auto.offset.reset", "latest");
         DataStream<String> stream = env
-                .addSource(new FlinkKafkaConsumer010<>("bi_dfp_order_rank_list", new SimpleStringSchema(), properties));
+                .addSource(new FlinkKafkaConsumer010<>("src_topic", new SimpleStringSchema(), properties));
         return stream;
     }
 
     public static FlinkKafkaProducer010 getKafkaSink() {
-        return new FlinkKafkaProducer010("kafkasit02broker01.cnsuning.com:9092,kafkasit02broker02.cnsuning.com:9092," +
-                "kafkasit02broker03.cnsuning.com:9092", "bi_dfp_oms_serv_unit", new SimpleStringSchema());
+        return new FlinkKafkaProducer010("broker", "target_topic", new SimpleStringSchema());
     }
 }
