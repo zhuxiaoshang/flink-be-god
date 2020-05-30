6186b857d299e119d1d9df55e4896dcd92e52e2d
zhushang <17626017841@163.com>
2020-05-31 01:59:42 +0800

    congratulations！ sink to hbase can work now!

--- flink-sql/src/main/java/sql/operator/SinkToHbase.java
+++ flink-sql/src/main/java/sql/operator/SinkToHbase.java
@@ -4,10 +4,33 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
-import sql.sink.ESSink;
 import sql.sink.HbaseSink;
 import sql.source.KafkaSource;
 
+/**
+ * 从kafka读数据写入hbase中
+ * 这里涉及到搭建hadoop、hbase、zk环境的过程，hbase不使用内置的zk，因为kafka也需要用zk,所以让他们公用一个zk，涉及到很多配置比较麻烦
+ * 比较重要的几个配置入下：
+ * 1.hadoop的etc/hadoop/core-site.xml
+ *   <property>
+ *     <name>fs.defaultFS</name>
+ *     <value>hdfs://localhost:9000</value>
+ *   </property>
+ * 2. hbase的conf/hbase-env.sh
+ * export HBASE_MANAGES_ZK=false   false表示使用外部zk
+ * 3. hbase的conf/hbase-site.xml
+ * <property>
+ *     <name>hbase.rootdir</name>
+ *     //这里设置让HBase存储文件的地方,要和上面hadoop的fs.defaultFS一致
+ *     <value>hdfs://localhost:9000/user/hbase</value>
+ *   </property>
+ *    <property>
+ *         <name>hbase.cluster.distributed</name>
+ *         <value>true</value>//同样表示使用外部zk
+ *     </property>
+ *
+ *  <p>详细配置我会上传到仓库</p>
+ */
 public class SinkToHbase {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -15,7 +38,7 @@ public class SinkToHbase {
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
         KafkaSource.getKafkaSource(tableEnv);//create kafka source table user_behavior
         HbaseSink.getHbaseSink(tableEnv);
-        Table table1 = tableEnv.sqlQuery("SELECT concat(user_id,item_id) as row_key,ROW(user_id, item_id,category_id," +
+        Table table1 = tableEnv.sqlQuery("SELECT concat(cast(user_id as String),cast(item_id as String)) as row_key,ROW(user_id, item_id,category_id," +
                 "behavior) as col_family" +
                 "\n" +
                 "FROM user_behavior\n" +
@@ -23,4 +46,82 @@ public class SinkToHbase {
         table1.insertInto("hbaseSinkTable");
         env.execute();
     }
+    /**
+     * 结果
+     * hbase(main):007:0> scan 'hbase_test',{LIMIT=>10}
+     * ROW                                                  COLUMN+CELL
+     *  10000033557403                                      column=cf:behavior, timestamp=1590860596937, value=buy
+     *  10000033557403                                      column=cf:category_id, timestamp=1590860596937,
+     *  value=\x00\x00\x00\x00\x00\x06F\x11
+     *  10000033557403                                      column=cf:item_id, timestamp=1590860596937,
+     *  value=\x00\x00\x00\x00\x006H\x1B
+     *  10000033557403                                      column=cf:user_id, timestamp=1590860596937,
+     *  value=\x00\x00\x00\x00\x00\x0FBC
+     *  10000173722681                                      column=cf:behavior, timestamp=1590860595873, value=buy
+     *  10000173722681                                      column=cf:category_id, timestamp=1590860595873,
+     *  value=\x00\x00\x00\x00\x00\x1BFv
+     *  10000173722681                                      column=cf:item_id, timestamp=1590860595873,
+     *  value=\x00\x00\x00\x00\x008\xCD\xB9
+     *  10000173722681                                      column=cf:user_id, timestamp=1590860595873,
+     *  value=\x00\x00\x00\x00\x00\x0FBQ
+     *  10000224910145                                      column=cf:behavior, timestamp=1590860597805, value=buy
+     *  10000224910145                                      column=cf:category_id, timestamp=1590860597805,
+     *  value=\x00\x00\x00\x00\x00 \x19\xD8
+     *  10000224910145                                      column=cf:item_id, timestamp=1590860597805,
+     *  value=\x00\x00\x00\x00\x00J\xECA
+     *  10000224910145                                      column=cf:user_id, timestamp=1590860597805,
+     *  value=\x00\x00\x00\x00\x00\x0FBV
+     *  10000514560771                                      column=cf:behavior, timestamp=1590860603925, value=buy
+     *  10000514560771                                      column=cf:category_id, timestamp=1590860603925,
+     *  value=\x00\x00\x00\x00\x00\x15\x00f
+     *  10000514560771                                      column=cf:item_id, timestamp=1590860603925,
+     *  value=\x00\x00\x00\x00\x00E\x97\x83
+     *  10000514560771                                      column=cf:user_id, timestamp=1590860603925,
+     *  value=\x00\x00\x00\x00\x00\x0FBs
+     *  10000543916598                                      column=cf:behavior, timestamp=1590860607024, value=buy
+     *  10000543916598                                      column=cf:category_id, timestamp=1590860607024,
+     *  value=\x00\x00\x00\x00\x00F;\xD9
+     *  10000543916598                                      column=cf:item_id, timestamp=1590860607024,
+     *  value=\x00\x00\x00\x00\x00;\xC36
+     *  10000543916598                                      column=cf:user_id, timestamp=1590860607024,
+     *  value=\x00\x00\x00\x00\x00\x0FBv
+     *  100007894989                                        column=cf:behavior, timestamp=1590860608109, value=buy
+     *  100007894989                                        column=cf:category_id, timestamp=1590860608109,
+     *  value=\x00\x00\x00\x00\x00\x1E\xCE\x15
+     *  100007894989                                        column=cf:item_id, timestamp=1590860608109,
+     *  value=\x00\x00\x00\x00\x00\x01s\x0D
+     *  100007894989                                        column=cf:user_id, timestamp=1590860608109,
+     *  value=\x00\x00\x00\x00\x00\x0FB\x8E
+     *  10000842086401                                      column=cf:behavior, timestamp=1590860604390, value=buy
+     *  10000842086401                                      column=cf:category_id, timestamp=1590860604390,
+     *  value=\x00\x00\x00\x00\x00\x00\xFFR
+     *  10000842086401                                      column=cf:item_id, timestamp=1590860604390,
+     *  value=\x00\x00\x00\x00\x00\x1F\xD6\x01
+     *  10000842086401                                      column=cf:user_id, timestamp=1590860604390,
+     *  value=\x00\x00\x00\x00\x00\x0FB\x94
+     *  10000842675133                                      column=cf:behavior, timestamp=1590860604390, value=buy
+     *  10000842675133                                      column=cf:category_id, timestamp=1590860604390,
+     *  value=\x00\x00\x00\x00\x00\x00\xFFR
+     *  10000842675133                                      column=cf:item_id, timestamp=1590860604390,
+     *  value=\x00\x00\x00\x00\x00(\xD1\xBD
+     *  10000842675133                                      column=cf:user_id, timestamp=1590860604390,
+     *  value=\x00\x00\x00\x00\x00\x0FB\x94
+     *  10000843350364                                      column=cf:behavior, timestamp=1590860606832, value=buy
+     *  10000843350364                                      column=cf:category_id, timestamp=1590860606832,
+     *  value=\x00\x00\x00\x00\x00;|s
+     *  10000843350364                                      column=cf:item_id, timestamp=1590860606832,
+     *  value=\x00\x00\x00\x00\x003\x1F\x5C
+     *  10000843350364                                      column=cf:user_id, timestamp=1590860606832,
+     *  value=\x00\x00\x00\x00\x00\x0FB\x94
+     *  10000881628033                                      column=cf:behavior, timestamp=1590860612671, value=buy
+     *  10000881628033                                      column=cf:category_id, timestamp=1590860612671,
+     *  value=\x00\x00\x00\x00\x00,\x08\x0A
+     *  10000881628033                                      column=cf:item_id, timestamp=1590860612671,
+     *  value=\x00\x00\x00\x00\x00\x18\xD7\x81
+     *  10000881628033                                      column=cf:user_id, timestamp=1590860612671,
+     *  value=\x00\x00\x00\x00\x00\x0FB\x98
+     * 10 row(s)
+     * Took 0.1680 seconds
+     */
+
 }
--- flink-sql/src/main/java/sql/sink/HbaseSink.java
+++ flink-sql/src/main/java/sql/sink/HbaseSink.java
@@ -5,29 +5,20 @@ import org.apache.flink.table.api.java.StreamTableEnvironment;
 public class HbaseSink {
     public static void getHbaseSink(StreamTableEnvironment tableEnvironment) {
         tableEnvironment.sqlUpdate("CREATE TABLE hbaseSinkTable (\n" +
-                "  row_key String,\n" +
-                "  col_family ROW<user_id BIGINT,item_id BIGINT,category_id BIGINT,behavior STRING>\n" +
+                "    row_key string,\n" +
+                "    cf ROW(user_id BIGINT,item_id BIGINT,category_id BIGINT,behavior STRING)\n" +
                 ") WITH (\n" +
-                "  'connector.type' = 'hbase', -- required: specify this table type is hbase\n" +
-                "  \n" +
-                "  'connector.version' = '1.2.6',          -- required: valid connector versions are \"1.4.3\"\n" +
-                "  'connector.table-name' = 'dfp_mem_lable_score_inf',  -- required: hbase table name\n" +
-                "  \n" +
-                "  'connector.zookeeper.quorum' = 'namenode1-sit.cnsuning.com,namenode2-sit.cnsuning.com,slave01-sit.cnsuning.com', -- required: HBase Zookeeper quorum configuration\n" +
-                "  'connector.zookeeper.znode.parent' = '/hbase',    -- optional: the root dir in Zookeeper for HBase cluster.\n" +
-                "                                                   -- The default value is \"/hbase\".\n" +
-                "\n" +
-                "  'connector.write.buffer-flush.max-size' = '10mb', -- optional: writing option, determines how many size in memory of buffered\n" +
-                "                                                    -- rows to insert per round trip. This can help performance on writing to JDBC\n" +
-                "                                                    -- database. The default value is \"2mb\".\n" +
-                "\n" +
-                "  'connector.write.buffer-flush.max-rows' = '1000', -- optional: writing option, determines how many rows to insert per round trip.\n" +
-                "                                                    -- This can help performance on writing to JDBC database. No default value,\n" +
-                "                                                    -- i.e. the default flushing is not depends on the number of buffered rows.\n" +
-                "\n" +
-                "  'connector.write.buffer-flush.interval' = '2s',   -- optional: writing option, sets a flush interval flushing buffered requesting\n" +
-                "                                                    -- if the interval passes, in milliseconds. Default value is \"0s\", which means\n" +
-                "                                                    -- no asynchronous flush thread will be scheduled.\n" +
+                "    'connector.type' = 'hbase',\n" +
+                "    -- 目前只支持 1.4.3 ，   HBaseValidator.CONNECTOR_VERSION_VALUE_143 写死了 1.4.3, 其他版本可以正常写数到 " +
+                "hbase\n" +
+                "    -- 生产慎用\n" +
+                "    'connector.version' = '1.4.3',                    -- hbase vesion\n" +
+                "    'connector.table-name' = 'hbase_test',                  -- hbase table name\n" +
+                "    'connector.zookeeper.quorum' = 'localhost:2181',       -- zookeeper quorum\n" +
+                "    'connector.zookeeper.znode.parent' = '/hbase',    -- hbase znode in zookeeper\n" +
+                "    'connector.write.buffer-flush.max-size' = '10mb', -- max flush size\n" +
+                "    'connector.write.buffer-flush.max-rows' = '1000', -- max flush rows\n" +
+                "    'connector.write.buffer-flush.interval' = '2s'    -- max flush interval\n" +
                 ")");
     }
 }
